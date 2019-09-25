fcc5bc376ac07e17a742ac652e49d81504b062c1
Aleksandr Yulin <aleksandr.yulin@streamcommon.com>
2019-09-25 22:53:40 +0300

    added container component

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the streamcommon/console-runner package, a StreamCommon open software project.
+ *
+ * @copyright (c) 2019 StreamCommon Team
+ * @see https://github.com/streamcommon/console-runner
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Streamcommon\Console\Exception;
+
+/**
+ * Interface ExceptionInterface
+ *
+ * @package Streamcommon\Console\Exception
+ */
+interface ExceptionInterface
+{
+}
